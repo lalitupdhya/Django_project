a3767fe1ec3a0e2b13915ac005b4f737492fbf75
Lalit Upadhyaya <lalitupdhya1992@gmail.com>
2020-04-14 21:16:43 +0530

    Initial Commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lalit/House_Listing_Django_Project/dev/btre_project/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
